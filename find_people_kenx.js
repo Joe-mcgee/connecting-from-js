4e58396e04fcb4963dc257773f9ce5a4f403d062
Taylor <thulsmans.2994@me.com>
2018-02-06 21:11:13 +0000

    functional find_people_kenx.js file

--- find_people_kenx.js
+++ find_people_kenx.js
@@ -16,10 +16,8 @@ let search = knex.select().from('famous_people').where({
   last_name: query
 }).then(function(rows) {
   console.log(rows.length + " person(s) by the name of '" + query + "': ");
-    for (row in rows) {
+  for (row in rows) {
     console.log('- ' + Object.values(rows[row])[0] + ': ' + Object.values(rows[row])[1] + ' ' + Object.values(rows[row])[2] + ', ' + "born, '" + Object.values(rows[row])[3] + "'");
   }
-/*  console.log(Object.values(results[0]));*/
+  /*  console.log(Object.values(results[0]));*/
 }).asCallback();
-
-
